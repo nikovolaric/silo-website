5c5ee751c03478f5e1aed086f55e0f0bc4eb7f4b
nikovolaric <niko.volaric@gmail.com>
2024-12-16 13:08:19 +0100

    12.12. popravki

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,5 @@
 import { Metadata } from "next";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import LoginForm from "../_components/LoginForm";
-import User from "../_models/userModel";
-import { jwtDecode } from "jwt-decode";
 
 export const metadata: Metadata = {
   title: "Vpis",
@@ -12,15 +8,6 @@ export const metadata: Metadata = {
 export const dynamic = "force-dynamic";
 
 async function Page() {
-  const session = cookies().get("jwt")?.value;
-  if (session) {
-    const { id: userId }: { id: string } = await jwtDecode(session);
-    const user = await User.findById(userId);
-    if (user && user.role === "admin") {
-      redirect("/dashboard");
-    }
-  }
-
   return (
     <>
       <div className="bg-neutral">
