e163feb1908cb048852859eb2d0df1343ca401b9
nikovolaric <niko.volaric@gmail.com>
2025-02-19 14:17:38 +0100

    Performance imporvements

--- app/_components/CookiesFirstPage.tsx
+++ app/_components/CookiesFirstPage.tsx
@@ -15,25 +15,25 @@ function CookiesFirstPage({ srb, slo }: { srb?: boolean; slo?: boolean }) {
   return (
     <>
       {isOpen && (
-        <div className="sticky bottom-0 z-50 rounded-t-lg bg-white/90 py-4 animate-cookieAni text-secondary px-4 md:px-8 xl:mx-auto max-w-6xl flex flex-col gap-5">
+        <div className="fixed bottom-0 left-1/2 z-50 flex w-11/12 max-w-6xl -translate-x-1/2 flex-col gap-5 rounded-t-lg bg-white/90 px-4 py-4 md:px-8">
           <div className="flex justify-end">
             <XMarkIcon className="h-6 cursor-pointer" onClick={handleSubmit} />
           </div>
-          <p className="font-raj font-semibold text-lg">
+          <p className="font-raj text-lg font-semibold">
             {slo
               ? "Spletno mesto uporablja piškotke za zagotavljanje boljše uporabniške izkušnje in spremljanje statistike obiska."
               : srb
-              ? "Veb lokacija koristi kolačiće kako bi pružila bolje korisničko iskustvo i pratila statistiku poseta."
-              : "This website uses cookies to improve your browsing experience and to track website traffic."}
+                ? "Veb lokacija koristi kolačiće kako bi pružila bolje korisničko iskustvo i pratila statistiku poseta."
+                : "This website uses cookies to improve your browsing experience and to track website traffic."}
           </p>
-          <div className="flex gap-5 items-center justify-center md:justify-start">
+          <div className="flex items-center justify-center gap-5 md:justify-start">
             <form action={acceptAllCookies} onSubmit={handleSubmit}>
-              <button className="text-primary border border-primary font-semibold text-sm px-4 py-0.5 rounded-full transition-colors duration-200 hover:bg-neutral">
+              <button className="rounded-full border border-primary px-4 py-0.5 text-sm font-semibold text-primary transition-colors duration-200 hover:bg-neutral">
                 {slo ? "Sprejmi vse" : srb ? "Prihvati sve" : "Accept all"}
               </button>
             </form>
             <form action={declineAllCookies} onSubmit={handleSubmit}>
-              <button className="text-primary border border-primary font-semibold text-sm px-4 py-0.5 rounded-full transition-colors duration-200 hover:bg-neutral">
+              <button className="rounded-full border border-primary px-4 py-0.5 text-sm font-semibold text-primary transition-colors duration-200 hover:bg-neutral">
                 {slo ? "Zavrni vse" : srb ? "Odbij sve" : "Decline all"}
               </button>
             </form>
@@ -42,8 +42,8 @@ function CookiesFirstPage({ srb, slo }: { srb?: boolean; slo?: boolean }) {
             {slo
               ? "Za nastavitve in več informacij sledite "
               : srb
-              ? "Za podešavanja i više informacija pratite "
-              : "For Settings and more information, please follow the "}
+                ? "Za podešavanja i više informacija pratite "
+                : "For Settings and more information, please follow the "}
             <Link
               href={slo ? "/si/piskotki" : srb ? "/rs/kolacici" : "/cookies"}
               className="underline"
